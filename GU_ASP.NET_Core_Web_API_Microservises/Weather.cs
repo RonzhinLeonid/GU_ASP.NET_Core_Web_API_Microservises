a5ae3ebfc8fd36a6f337572242f55ad13643d014
Leonid <jiehuh87@yandex.ru>
2021-04-24 23:15:13 +0300

    Les1

---
+++ GU_ASP.NET_Core_Web_API_Microservises/Weather.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GU_ASP.NET_Core_Web_API_Microservises
+{
+    public class Weather
+    {
+        public DateTime Date { get; set; }
+        public int Temperature{ get; set; }
+
+    }
+}
