9aac6c19aa3ccadb944bced79e919bee84549f1b
Leonid <jiehuh87@yandex.ru>
2021-04-26 00:12:22 +0300

    Заготовка для дз

---
+++ GU_ASP.NET_Core_Web_API_Microservises/ValuesHolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GU_ASP.NET_Core_Web_API_Microservises
+{
+    public class ValuesHolder
+    {
+        List<string> values = new List<string>();
+        public List<string> Values { get; set; }
+
+        public void AddValue(string value)
+        {
+            values.Add(value);
+        }
+    }
+}
